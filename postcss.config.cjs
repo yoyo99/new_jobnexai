e562da9192a530ae4718cb5f9b9eb0aef79b3c31
yoyo99 <lauboin@free.fr>
2026-02-17 21:04:21 +0100

    Mise à jour des configurations et des dépendances

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: {
-    tailwindcss: { config: './tailwind.config.cjs' },
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('tailwindcss')({ config: './tailwind.config.cjs' }),
+    require('autoprefixer'),
+  ],
+};
