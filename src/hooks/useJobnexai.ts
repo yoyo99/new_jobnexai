4b0913e60154f88a7a530c686d72d646daafad53
Yoyo99 <lauboin@free.fr>
2026-02-22 17:25:21 +0100

    fix: Remove unused jobActions variable from useJobnexai hook
    - Clean up TypeScript warning
    - Remove unused jobActions useMemo
    - Code is now lint-free

--- src/hooks/useJobnexai.ts
+++ src/hooks/useJobnexai.ts
@@ -56,34 +56,7 @@ export function useJobnexai() {
     auth.signInWithGoogle
   ]);
   
-  // Exposer les fonctions de gestion des offres d'emploi
-  const jobActions = useMemo(() => ({
-    // Recherche et navigation
-    getJobs: jobs.getJobs,
-    getJobById: jobs.getJobById,
     
-    // Actions employeur
-    createJob: jobs.createJob,
-    updateJob: jobs.updateJob,
-    deleteJob: jobs.deleteJob,
-    getApplicationsForJob: jobs.getJobApplications,
-    updateApplicationStatus: jobs.updateApplicationStatus,
-    
-    // Actions candidat
-    applyToJob: jobs.applyToJob,
-    getMyApplications: jobs.getMyApplications,
-  }), [
-    jobs.getJobs,
-    jobs.getJobById,
-    jobs.createJob,
-    jobs.updateJob,
-    jobs.deleteJob,
-    jobs.getJobApplications,
-    jobs.updateApplicationStatus,
-    jobs.applyToJob,
-    jobs.getMyApplications
-  ]);
-  
   // Exposer les fonctions utilitaires API
   const apiActions = useMemo(() => ({
     checkAPIStatus: netlify.checkAuth,
